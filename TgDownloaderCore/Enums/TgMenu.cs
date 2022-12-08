2a068e071d2c945b463436a15e2d765301cb9726
Damian Viktorovich Morozov <damian@morozov33.ru>
2022-12-08 22:24:47 +0300

    Added TgDownloaderCore project

---
+++ TgDownloaderCore/Enums/TgMenu.cs
@@ -0,0 +1,17 @@
+﻿// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+namespace TgDownloaderCore.Enums;
+
+public enum TgMenu
+{
+    Exit,
+    Return,
+    DownloadFiles,
+    SetupTg,
+    SetupTgDestDirectory,
+    SetupTgChannelUsername,
+    SetupTgClient,
+    SetupTgMessageCount,
+    SetupTgMessageStartId,
+}
