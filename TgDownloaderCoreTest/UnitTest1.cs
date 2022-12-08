d2c52bfe9a839a983c117c83090f2d088b3925f2
Damian Viktorovich Morozov <damian@morozov33.ru>
2022-12-08 22:25:11 +0300

    Added TgDownloaderCoreTest project

---
+++ TgDownloaderCoreTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace TgDownloaderCoreTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
