5f5c95fca942e4eff72a5167c6012d937c77f9c1
Damian Viktorovich Morozov <damian@morozov33.ru>
2024-11-05 23:05:41 +0400

    Updated TgStorageTest

--- Tests/TgStorageTest/Utils/TgStorageTestsUtils.cs
+++ Tests/TgStorageTest/Utils/TgStorageTestsUtils.cs
@@ -1,5 +1,6 @@
 ﻿// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+#pragma warning disable NUnit1033
 
 namespace TgStorageTest.Utils;
 
